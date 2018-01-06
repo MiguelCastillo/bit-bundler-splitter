c4d797f3aa86f8289cb4d42dd00ddab1ae438ff6
Miguel Castillo <manchagnu@gmail.com>
2018-01-06 16:49:51 -0500

    tweaked example confguration to use strings instead of regexes

--- examples/renderer/.bitbundler.js
+++ examples/renderer/.bitbundler.js
@@ -6,11 +6,10 @@ module.exports = {
   loader: [
     "bit-loader-babel"
   ],
-
   bundler: [
     ["bit-bundler-splitter", [
-      { name: "vendor", dest: "dist/vendor.js", match: { path: /\/node_modules\// } },
-      { name: "renderer", dest: "dist/renderer.js", match: { path: /\/src\/renderer\// } }
-    ]]
+      { name: "vendor", dest: "dist/vendor.js", match: ["/bower_components/", "/node_modules/"] },
+      { name: "renderer", dest: "dist/renderer.js", match: "/src/renderer/" } ]
+    ]
   ]
 };
